0d9b63c3b2de02ed9f8ee4411cdb73b233e17d9c
Muhammad Imran Siddique <muimsd@gmail.com>
2025-03-16 16:17:26 +0500

    refactor DisplayArea component layout; center content and adjust spacing for improved readability

--- examples/maplibre/src/DisplayArea.tsx
+++ examples/maplibre/src/DisplayArea.tsx
@@ -15,10 +15,10 @@ export const DisplayArea = ({ area }: { area: number }) => {
             justifyContent: 'center',
             flexDirection: 'column'
         }}>
-            <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', padding: '0px 2px', margin: 0 }}>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'center', padding: '0px 2px', margin: 0 }}>
                 <p style={{ color: 'white', margin: 0, padding: 0 }}><span style={{ color: area >= 200 ? 'red' : 'green' }}>Area: <span id="clipped-polygon-area">{area}</span> km<sup>2</sup></span> Max area 200 km<sup>2</sup></p>
             </div>
-            <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', padding: '5px 2px' }}>
+            <div style={{ width: '100%', display: 'flex', justifyContent: 'center', padding: '5px 2px' }}>
                 <input type="text" placeholder="Input area name" style={{
                     display: 'block',
                     width: '100%',
